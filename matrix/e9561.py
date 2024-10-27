adef57f86e4a66d006b726e90584ae2470be608a
sanyache <sanyache75@gmail.com>
2024-10-27 19:01:57 +0200

    e9561

--- matrix/e9561.py
+++ matrix/e9561.py
@@ -10,3 +10,4 @@ n,m = map(int, input().split())
 matrix = [[int(x) for x in input().split()] for _ in range(n) ]
 transposed_matrix = [max([row[i] for row in matrix]) for i in range(m)]
 print(*transposed_matrix)
+
--- valid_email/valid_email_api.py
+++ valid_email/valid_email_api.py
@@ -1,11 +1,30 @@
 import  requests
 import json
+
+class CantGetAPI(Exception):
+    """unable to receive data from api"""
+    def __str__(self):
+        return "unable to receive data from api"
+
+
 # licey.lit@gmail.com
 def is_valid_email(email):
-    response = requests.get(f' https://api.2ip.ua/email.json?email={email}')
-    email_api = response.json()
-    print(json.dumps(email_api, indent=4, sort_keys=True))
-    return email_api['exist']
+    try:
+        response = requests.get(f' https://api.2ip.ua/email.json?email={email}')
 
-str_email = input()
-print("Yes" if is_valid_email(str_email) else "No")
+    except:
+        raise CantGetAPI
+    else:
+        email_api = response.json()
+        print(json.dumps(email_api, indent=4, sort_keys=True))
+        return email_api['exist']
+
+
+if __name__ == "__main__":
+    str_email = input("Enter email address ")
+    try:
+        is_valid = is_valid_email(str_email)
+    except CantGetAPI as e:
+        print(f"Error: {e}")
+    else:
+        print("Yes" if is_valid else "No")
