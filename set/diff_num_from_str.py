685d0d0674c31418a7bba8da16784a518fc6b3bd
sanyache <sanyache75@gmail.com>
2024-12-15 14:12:17 +0200

    diff num

---
+++ set/diff_num_from_str.py
@@ -0,0 +1,15 @@
+"""
+Дано натуральне число n. Визначити скільки різних трьохзначних чисел може
+утворитися видаленням 3 цифр з цого десяткового представлення
+12 - 0
+111111111110011111111  - 4
+"""
+from itertools import combinations
+
+s = input()
+if len(s) < 3:
+    print(0)
+    exit()
+combs = list(combinations(s, 3))
+set_num = {x for x in combs if 99 < int(''.join(x)) < 1000}
+print(len(set_num))
